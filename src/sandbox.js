5a8c21906911bf66563be314ca8c87d809d18a9f
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-08 09:29:40 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24262 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/sandbox.js
+++ src/sandbox.js
@@ -1,77 +1,4 @@
 
-function Configuration(params) {
-  if(!(this instanceof Configuration)) return new Configuration(params);
-
-  //                      _   _                      _    
-  // _ __  __ _ _________| |_| |_  _ _ ___ _  _ __ _| |_  
-  //| '_ \/ _` (_-<_-<___|  _| ' \| '_/ _ \ || / _` | ' \ 
-  //| .__/\__,_/__/__/    \__|_||_|_| \___/\_,_\__, |_||_|
-  //|_|                                        |___/      
-
-  var passthrough = (params["passthrough"]) ? true : false;
-
-  Object.defineProperty(this, "passthrough", {
-    get: function () { return passthrough; },
-    enumerable: true
-  });
-}
-
-function Evaluation(params) {
-  if(!(this instanceof Evaluation)) return new Evaluation(params);
-
-  //    _                       _ _     
-  // __| |___ __ ___ _ __  _ __(_) |___ 
-  /// _` / -_) _/ _ \ '  \| '_ \ | / -_)
-  //\__,_\___\__\___/_|_|_| .__/_|_\___|
-  //                      |_|           
-
-  var decompile = (params["decompile"]) ? true : false;
-
-  Object.defineProperty(this, "decompile", {
-    get: function () { return decompile; },
-    enumerable: true
-  });
-
-  //                 _                      
-  // _ __  ___ _ __ | |__ _ _ __ _ _ _  ___ 
-  //| '  \/ -_) '  \| '_ \ '_/ _` | ' \/ -_)
-  //|_|_|_\___|_|_|_|_.__/_| \__,_|_||_\___|
-
-  var membrane = (params["membrane"]) ? true : false;
-
-  Object.defineProperty(this, "membrane", {
-    get: function () { return membrane; },
-    enumerable: true
-  });
-}
-
-function Verbose(params) {
-  if(!(this instanceof Verbose)) return new Verbose(params);
-
-  //                 _                      
-  // _ __  ___ _ __ | |__ _ _ __ _ _ _  ___ 
-  //| '  \/ -_) '  \| '_ \ '_/ _` | ' \/ -_)
-  //|_|_|_\___|_|_|_|_.__/_| \__,_|_||_\___|
-
-  var membrane = (params["membrane"]) ? true : false;
-
-  Object.defineProperty(this, "membrane", {
-    get: function () { return membrane; },
-    enumerable: true
-  });
-
-  // _                             _   _             
-  //| |_ _ _ __ _ _ _  ___ __ _ __| |_(_)___ _ _  ___
-  //|  _| '_/ _` | ' \(_-</ _` / _|  _| / _ \ ' \(_-<
-  // \__|_| \__,_|_||_/__/\__,_\__|\__|_\___/_||_/__/
-
-  var transactions = (params["transactions"]) ? true : false;
-
-  Object.defineProperty(this, "transactions", {
-    get: function () { return transactions; },
-    enumerable: true
-  });
-}
 
 
 
@@ -85,48 +12,59 @@ function ShellOut() {
   if(!(this instanceof ShellOut)) return new ShellOut();
   else Out.call(this);
 
-  // TODO: set verbose configutarion
-  //
+  var verbose = {
+    membrane:false,
+    transactions:false
+  };
+
+  this.configure = function(params) {
+    for(arg in verbose) {
+      if(params[arg]) verbose[arg]=params[arg];
+    }
+  }
+
+  /** Padding Information
+   */
   var idWidth = 30;
   var fstWidth = 100;
   var sndWidth = 20;
   var seperator = ".";
 
-  /** Standard Output
+  /** Standard Output (Head + Message)
   */
   function out(string) {
     putstr(padding_right(string + " ", seperator, fstWidth));
   }
 
-  /** Sub-Standard Output
+  /** Sub-Standard Output (Message)
   */
   function subout(string) {
     putstr(padding_right("... " + string + " ", seperator, fstWidth));
   }
 
-  /** BLANK Output
+  /** Blank Output (End Of Line)
   */
   function blank() {
     putstr(padding_left(seperator, seperator, sndWidth));
     putstr("\n");
   }
 
-  /** Notice Output
+  /** Notice Output (Sub-Output including Blank)
   */
   function notice(string) {
     putstr(padding_right("... " + string + " ", seperator, fstWidth+sndWidth));
     putstr("\n");
   }
 
-   /** Head Output
+  /** Head Output
   */
   function head(id) {
-    return padding_right(id + " ", ".", idWidth)
+    return padding_right(id + " ", ".", idWidth);
   }
-
-
-
-
+  //                 _                      
+  // _ __  ___ _ __ | |__ _ _ __ _ _ _  ___ 
+  //| '  \/ -_) '  \| '_ \ '_/ _` | ' \/ -_)
+  //|_|_|_\___|_|_|_|_.__/_| \__,_|_||_\___|
 
   this.membrane = function(msg) {
     if(verbose.membrane) {
@@ -135,8 +73,13 @@ function ShellOut() {
     }
   };
 
+  // _                             _   _             
+  //| |_ _ _ __ _ _ _  ___ __ _ __| |_(_)___ _ _  ___
+  //|  _| '_/ _` | ' \(_-</ _` / _|  _| / _ \ ' \(_-<
+  // \__|_| \__,_|_||_/__/\__,_\__|\__|_\___/_||_/__/
+
   this.transactions = function(msg) {
-    if(verbose.membrane) {
+    if(verbose.transactions) {
       __out(head("[Transaction]") + msg);
       __blank();
     }
@@ -145,24 +88,63 @@ function ShellOut() {
 ShellOut.prototype = new Out();
 
 
+/*
+ * out
+ *
+ * verbose
+ * - membrane
+ * - transactions
+ *
+ * configuration
+ * - nativepassthrough
+ *
+ * evaluation
+ * - decompile
+ * - membrane
+ *
+ *
+ *
+ *
+ */
 
 
 
 
+function Sandbox(out, verbose, configuration, evluation) {
+  if(!(this instanceof Sandbox)) return new Sandbox(out, verbose, configuration, evluation);
 
-function Sandbox(
-    configuration, 
-    verbose,
-    log
-    ) {
+var uid = this.getSbxId();
 
+this.id = uid;
+
+  // TODO, is
+  //
+
+this.bind = function() {};
       // TODO defualt
-
-
 
 
     }
 
+Sandbox.prototype.getSbxId = (function() {
+  var str = "sbx-";
+  var i = 0;
+  return function() {
+    i = i+1;
+    return (str+(padding_left(String(i), "0", 3)));
+  }
+})();
+
+//(new Out()).id;
+
+var sbx = new Sandbox();
+sbx.bind();
+sbx.bind();
+print("###1" + sbx.id);
+
+var sbx2 = new Sandbox();
+sbx2.bind();
+sbx2.bind();
+print("###2" + sbx2.id);
 
 
-var sbx = new Sandbox({membrane:true;}, {});
