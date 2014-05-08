e76bc5b4ae08343afd2804225941af251f933334
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-08 12:55:27 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24267 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- src/sandbox.js
+++ src/sandbox.js
@@ -169,13 +169,13 @@ function Sandbox(params) {
 
   var cache = new WeakMap();
 
-  /** wrap(target)
-   *
+  /** 
+   * wrap(target)
    * Wraps a target object.
    *
    * @param target Object
-   * @param global, list of allowed properties
-   * @return Membrane Reference/ Proxy 
+   * @param global Object
+   * @return JavaScript Proxy 
    */
   function wrap(target, global) { 
     logc("wrap");
@@ -185,19 +185,154 @@ function Sandbox(params) {
       return target;
     }
 
-
-    if(cache.contains(target)) {
+    // if target is already wrapped, return cached proxy
+    if(cache.has(target)) {
       return cache.get(target);
-    } else { 
-      var membraneHandler = new Membrabe(global);
-      var proxy = new Proxy(target, membraneHandler);
-
-      cache.put(target, proxy);
-
+    } else {
+      var proxy = new Proxy(target, new Membrane());
+      cache.set(target, proxy);
       return proxy;
     }
   }
 
+  // __  __           _                      
+  //|  \/  |___ _ __ | |__ _ _ __ _ _ _  ___ 
+  //| |\/| / -_) '  \| '_ \ '_/ _` | ' \/ -_)
+  //|_|  |_\___|_|_|_|_.__/_| \__,_|_||_\___|
+
+  /** Membrabe(global)
+   * Implements a membrane to evaluate functions in a sandbox
+   *
+   * @param, the sandboxs global object
+   */
+  function Membrabe(global) {
+    var scope = {};
+    this.getOwnPropertyDescriptor = function(target, name) {
+      log("[[getOwnPropertyDescriptor]]", name);
+      var desc = Object.getOwnPropertyDescriptor(target, name);
+      if (desc !== undefined) desc.value = wrap(desc, global);
+      return desc;
+    };
+    this.getOwnPropertyNames = function(target) {
+      log("[[getOwnPropertyNames]]", name);
+      return Object.getOwnPropertyNames(target);
+    };
+    this.getPrototypeOf = function(target) {
+      log("[[getPrototypeOf]]", name);
+      return Object.getPrototypeOf(target)
+    };
+    this.defineProperty = function(target, name, desc) {
+      log("[[defineProperty]]", name);
+      return Object.defineProperty(target, name, desc);
+    };
+    this.deleteProperty = function(target, name) {
+      log("[[deleteProperty]]", name);
+      return delete target[name];
+    };
+    this.freeze = function(target) {
+      log("[[freeze]]", name);
+      return Object.freeze(target);
+    };
+    this.seal = function(target) {
+      log("[[seal]]", name);
+      return Object.seal(target);
+    };
+    this.preventExtensions = function(target) {
+      log("[[preventExtensions]]", name);
+      return Object.preventExtensions(target);
+    };
+    this.isFrozen = function(target) {
+      log("[[isFrozen]]", name);
+      return Object.isFrozen(target);
+    };
+    this.isSealed = function(target) {
+      log("[[isSealed]]", name);
+      return Object.isSealed(target);
+    };
+    this.isExtensible = function(target) {
+      log("[[isExtensible]]", name);
+      return Object.isExtensible(target);
+    };
+    this.has = function(target, name) {
+      log("[[has]]", name);
+      // TODO
+      if(!(name in target)) violation("Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber);
+      else return (name in target);
+    };
+    this.hasOwn = function(target, name) {
+      log("[[hasOwn]]", name);
+
+      // TODO
+      if(!(name in target)) violation("Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber);
+      else return ({}).hasOwnProperty.call(target, name); 
+    };
+    this.get = function(target, name, receiver) {
+      log("[[get]]", name);
+
+       if (name in scope) {
+        return  wrap(scope[name], global);
+      } else if(name in target) {
+        if( _.Config.nativePassThrough) {
+          // pass-through of native functions
+          if(isNativeFunction(target[name])) {
+            return target[name];
+          }
+          else return wrap(target[name], global);
+        } else {
+          return wrap(target[name], global);
+        }
+
+      } else {
+        violation("Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber); 
+      }
+      
+      
+//      if(!(name in target)) violation("Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber);
+
+      // pass-through of Contract System
+      // if(name=="$") return target[name];
+
+/*      if( _.Config.nativePassThrough) {
+        // pass-through of native functions
+        if(isNativeFunction(target[name])) {
+          return target[name];
+        }
+        else return wrap(target[name], global);
+      } else {
+        return wrap(target[name], global);
+      }
+*/
+    };
+    this.set = function(target, name, value, receiver) {
+      log("[[set]]", name);
+      // NOTE: no write access allowed
+      //violation("XXXX Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber);
+      return (scope[name]=value);
+      //otherwise use this code:
+      //if(!(name in target)) violation("Unauthorized Access " + name, (new Error()).fileName, (new Error()).lineNumber);
+      //else return target[name] = value;
+    };
+    this.enumerate = function(target) {
+      log("[[enumerate]]", name);
+      var result = [];
+      for (var name in target) {
+        result.push(name);
+      };
+      return result;
+    };
+    this.keys = function(target) {
+      log("[[keys]]");
+      return Object.keys(target);
+    };
+    this.apply = function(target, thisArg, argsArray) {
+      log("[[apply]]");
+      return evalFunction(target, global, thisArg, argsArray);
+    };
+    this.construct = function(target, argsArray) {
+      log("[[construct]]");
+      return evalNew(target, global, this, argsArray);
+    };
+  };
 
 
 
