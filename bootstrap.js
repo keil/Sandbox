bd83129187505826fcaac108018fdfc09c99a70f
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-19 07:50:34 +0000

    A new demo implementatation of a switched membrane implementation.
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24337 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap.js
+++ bootstrap.js
@@ -22,25 +22,170 @@ load("src/sandbox.js");
 
 
 load('test/testcase.js');
-load('test/test.js');
+//load('test/test.js');
 
-var o = {a:1, b:2, c:3};
+/*
+   var target = {};
+   var handler = new Proxy({
+   get: function(target, name) { print("#"+name); return target[name]; },
+   XpreventExtensions: function(target) {
+   return 0;
+   },
+   XisExtensible: function(target) {
+   return false;
+   }
+   },{
+   get: function(target, name) { print("@"+name); return target[name]; }
+   });
+   var proxy = new Proxy(target, handler);
 
-var h = new Proxy({}, {
-  get: function(target, name) {
-  print("#" + name);
-  return target[name];
-  }
-})
+   print(proxy.a);
+   print(proxy.a=1);
+   print(proxy.a);
 
-var p = new Proxy(o, h);
-p.a;
-p.x=3;
+   print("---");
+   print("> "+Object.freeze(proxy));
+   print("---");
+   print("> "+Object.isFrozen(proxy));
+   print("---");
+   */
 
-for(x in p) {
-print("!" + x);
+
+
+(function() {
+  var target = {a:4711, b:4712, c:{x:4713, y:4714}};
+  var scope = {};
+
+  function Handler(base) {
+    this.get = function(target, name) {
+      return target[name];
+    }
+    this.set = function (target, name, value) {
+      return target[name]=value;
+    }
+    this.preventExtensions = function(target) {
+      return Object.preventExtensions(target);
+    },
+this.isExtensible = function(target) {
+  return Object.isExtensible(target);
+}
 }
 
+// wrap twice
+var proxy = new Proxy(target, new Handler(scope));
+
+print("---");
+print(proxy.a);
+print(proxy.b);
+print(proxy.c.x);
+print(proxy.c.y);
+print(">"+(proxy.c.z=1));
+print(proxy.c.z);
+print(">"+(proxy.x=1));
+print(proxy.x);
+print(proxy.y);
+print("---");
+
+for (p in proxy) {
+  print ("."+p);
+}
+
+print(">"+Object.preventExtensions(proxy));
+print(Object.isExtensible(target)===Object.isExtensible(proxy));
+
+for (p in target) {
+  print ("."+p);
+}
+
+
+})();
+
+(function() {
+  var target = {a:4711, b:4712, c:{x:4713, y:4714}};
+
+  function Handler(base, scope) {
+    for(p in base) {
+      scope[p]=base[p];
+    }
+
+    this.get = function(target, name) {
+      return wrap((name in target) ? target[name] : base[name]);
+    }
+    this.set = function (target, name, value) {
+      return target[name]=value;
+    }
+    this.preventExtensions = function(target) {
+      return Object.preventExtensions(target);
+    },
+  this.isExtensible = function(target) {
+    return Object.isExtensible(target);
+  }
+  }
+
+  var cache = new WeakMap();
+  function wrap(target) {
+    if(typeof target !== "object") return target;
+    if(cache.has(target)) {
+      var proxy = cache.get(target);
+    }
+    else {
+      var scope = {}
+      var proxy = new Proxy(scope, new Handler(target, scope));
+      cache.set(target, proxy);
+    }
+    return proxy;
+  }
+
+  var proxy = wrap(target);
+
+  print("---");
+  print(proxy.a);
+  print(proxy.b);
+  print(proxy.c.x);
+  print(proxy.c.y);
+  print(">"+(proxy.c.z=1));
+  print(proxy.c.z);
+  print(">"+(proxy.x=1));
+  print(proxy.x);
+  print(proxy.y);
+  print("---");
+
+  for (p in proxy) {
+    print ("."+p);
+  }
+
+  print(">"+Object.preventExtensions(proxy));
+  print(Object.isExtensible(target)===Object.isExtensible(proxy));
+
+  for (p in target) {
+    print ("."+p);
+  }
+
+
+})();
+
+
+
+
+
+/*
+   var o = {a:1, b:2, c:3};
+
+   var h = new Proxy({}, {
+   get: function(target, name) {
+   print("#" + name);
+   return target[name];
+   }
+   })
+
+   var p = new Proxy(o, h);
+   p.a;
+   p.x=3;
+
+   for(x in p) {
+   print("!" + x);
+   }
+   */
 
 
 //quit();
@@ -49,24 +194,24 @@ print("!" + x);
 /*
 
 
-  if(print) {
-    print("  _____             _      _ ___ ");
-    print(" |_   _| _ ___ __ _| |_ _ | / __|");
-    print("   | || '_/ -_) _` |  _| || \\__ \\");
-    print("   |_||_| \\___\\__,_|\\__|\\__/|___/");
-    print("                                 ");
+   if(print) {
+   print("  _____             _      _ ___ ");
+   print(" |_   _| _ ___ __ _| |_ _ | / __|");
+   print("   | || '_/ -_) _` |  _| || \\__ \\");
+   print("   |_||_| \\___\\__,_|\\__|\\__/|___/");
+   print("                                 ");
 
-    print(" * TreatJS: Sandbox");
-    print(" * http://proglang.informatik.uni-freiburg.de/treatjs/");
-    print("");
-    print(" * Copyright (c) 2014, Proglang, University of Freiburg.");
-    print(" * http://proglang.informatik.uni-freiburg.de/");
-    print(" * All rights reserved.");
-    print("");
-    print(" * Author Matthias Keil");
-    print(" * http://www.informatik.uni-freiburg.de/~keilr/");
-    print("");
-  }
+   print(" * TreatJS: Sandbox");
+   print(" * http://proglang.informatik.uni-freiburg.de/treatjs/");
+   print("");
+   print(" * Copyright (c) 2014, Proglang, University of Freiburg.");
+   print(" * http://proglang.informatik.uni-freiburg.de/");
+   print(" * All rights reserved.");
+   print("");
+   print(" * Author Matthias Keil");
+   print(" * http://www.informatik.uni-freiburg.de/~keilr/");
+   print("");
+   }
 
 
 */
@@ -75,8 +220,8 @@ print("!" + x);
 
 //(new Out()).id;
 /*
-var sbx = new Sandbox();
-sbx.id;
+   var sbx = new Sandbox();
+   sbx.id;
 //sbx.bind();
 //sbx.bind();
 print("###1" + sbx.id);
@@ -97,11 +242,11 @@ print(sbx2);
 
 var a = 1;
 function f(b, c, d) {
-  print("----");
-  print(this.a);
-  print(b);
-  print(c);
-  print(d);
+print("----");
+print(this.a);
+print(b);
+print(c);
+print(d);
 }
 
 f(4711,1);
@@ -111,30 +256,30 @@ g(4711, 1);
 */
 
 /**
-var x = 0;
-var y = 0;
-var z = 0;
+  var x = 0;
+  var y = 0;
+  var z = 0;
 
-function f (i) {
+  function f (i) {
   x=x+1;
   y=y+1;
   out(i);
-}
+  }
 
-function g (i) {
+  function g (i) {
   y=y+1;
   z=z+1;
   out(i);
-}
+  }
 
-function out(i) {
+  function out(i) {
   print("x@" + i + ": " + x);
   print("y@" + i + ": " + y);
   print("z@" + i + ": " +  z);
-}
+  }
 
-var a = new SandboxEnvironment(this);
-var b = new SandboxEnvironment(this);
+  var a = new SandboxEnvironment(this);
+  var b = new SandboxEnvironment(this);
 
 // TODO
 // 1) a.x sollte mir das x in a ausgeben
@@ -162,25 +307,25 @@ var b = new SandboxEnvironment(this);
 
 //f();
 //g();
-**/
+ **/
 /*
 
-ff = a.bind(f);
-ff("a");
-ff("a");
-ff("a");
-outa = a.bind(out);
+   ff = a.bind(f);
+   ff("a");
+   ff("a");
+   ff("a");
+   outa = a.bind(out);
 
-gg = b.bind(g);
-gg("b");
-gg("b");
-gg("b");
-outb = b.bind(out, this);
+   gg = b.bind(g);
+   gg("b");
+   gg("b");
+   gg("b");
+   outb = b.bind(out, this);
 
-print("\n--\n");
-out("global");
-outa("a");
-outb("b");
+   print("\n--\n");
+   out("global");
+   outa("a");
+   outb("b");
 
 */
 
@@ -192,42 +337,42 @@ outb("b");
 //
 
 /*
-print(":'(");
+   print(":'(");
 
-Object.defineProperty(Object.prototype, "id", {
-  get: (function() {
-    var i = 0;
+   Object.defineProperty(Object.prototype, "id", {
+   get: (function() {
+   var i = 0;
 
-    function makeID() {
-      i = i+1;
-      return (new String(i));
-    }
+   function makeID() {
+   i = i+1;
+   return (new String(i));
+   }
 
-    return function() {
-      var id = makeID();
+   return function() {
+   var id = makeID();
 
-      Object.defineProperty(this, "id", {
-        get: function() { return id; }});
+   Object.defineProperty(this, "id", {
+   get: function() { return id; }});
 
-      return id;
-    };
-  })()
-});
+   return id;
+   };
+   })()
+   });
 
-var o = new Object();
-print(o.id);
-print(o.id);
-print(o.id);
-print(o.id);
-var a = new Array();
-print(o.id);
-print(a.id);
-print(a.id);
-var oo = new Object();
-print(oo.id);
-print(o.id);
-print(a.id);
-print(oo.id);
+   var o = new Object();
+   print(o.id);
+   print(o.id);
+   print(o.id);
+   print(o.id);
+   var a = new Array();
+   print(o.id);
+   print(a.id);
+   print(a.id);
+   var oo = new Object();
+   print(oo.id);
+   print(o.id);
+   print(a.id);
+   print(oo.id);
 
 
 */
