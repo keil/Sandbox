7b9198b55c02655e0bcd6bf74cad72d2dc308bb8
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-20 19:17:19 +0000

    cleanup
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24361 e3db86c2-31ce-0310-bca5-be3edd1b5589

--- bootstrap.js
+++ bootstrap.js
@@ -22,7 +22,34 @@ load("src/sandbox.js");
 
 load('test/testcase.js');
 load('test/metahandler.js');
-load('test/test.js');
+
+//load('test/test.js');
+
+//                 _                       
+// _ __  ___ _ __ | |__  __ _ _ _ _ _  ___ 
+//| '  \/ -_) '  \| '_ \/ _` | '_| ' \/ -_)
+//|_|_|_\___|_|_|_|_.__/\__,_|_| |_||_\___|
+
+////load("test/membrane/Object.apply.js");
+////load("test/membrane/Object.construct.js");
+////load("test/membrane/Object.defineProperty.js");
+////load("test/membrane/Object.deleteProperty.js");
+////load("test/membrane/Object.enumerate.js");
+//load("test/membrane/Object.freeze.js");
+////load("test/membrane/Object.get.js"); 
+//load("test/membrane/Object.getOwnPropertyDescriptor.js");
+//load("test/membrane/Object.getOwnPropertyNames.js");
+//load("test/membrane/Object.getPrototypeOf.js"); // TODO should not be a testcase
+////load("test/membrane/Object.has.js");
+////load("test/membrane/Object.hasOwn.js");
+////load("test/membrane/Object.isExtensible.js");
+//load("test/membrane/Object.isFrozen.js");
+//load("test/membrane/Object.isSealed.js");
+////load("test/membrane/Object.iterate.js");
+////load("test/membrane/Object.keys.js"); 
+////load("test/membrane/Object.preventExtensions.js");
+load("test/membrane/Object.seal.js");
+////load("test/membrane/Object.set.js");
 
 
 (function() {
--- test/membrane/membrane.js
+++ test/membrane/membrane.js
@@ -18,23 +18,83 @@
 //| '  \/ -_) '  \| '_ \/ _` | '_| ' \/ -_)
 //|_|_|_\___|_|_|_|_.__/\__,_|_| |_||_\___|
 
-////load("test/membrane/Object.apply.js");
-////load("test/membrane/Object.construct.js");
-////load("test/membrane/Object.defineProperty.js");
-////load("test/membrane/Object.deleteProperty.js");
-////load("test/membrane/Object.enumerate.js");
-//load("test/membrane/Object.freeze.js");
-////load("test/membrane/Object.get.js"); 
-//load("test/membrane/Object.getOwnPropertyDescriptor.js");
-//load("test/membrane/Object.getOwnPropertyNames.js");
-//load("test/membrane/Object.getPrototypeOf.js"); // TODO should not be a testcase
-////load("test/membrane/Object.has.js");
-////load("test/membrane/Object.hasOwn.js");
-////load("test/membrane/Object.isExtensible.js");
+// Object.apply
+load("test/membrane/Object.apply.js");
+
+// Object.construct
+load("test/membrane/Object.construct.js");
+
+// Object.defineProperty
+load("test/membrane/Object.defineProperty.js");
+
+// Object.deleteProperty
+load("test/membrane/Object.deleteProperty.js");
+
+// Object.enumerate
+// Note: Proxy Trap ist not implemented
+load("test/membrane/Object.enumerate.js");
+
+// Object.freeze
+// Note: Proxy Trap ist not implemented
+// load("test/membrane/Object.freeze.js");
+
+// Object.get
+load("test/membrane/Object.get.js"); 
+
+// Object.getOwnPropertyDescriptor
+// TODO: Not tested
+// load("test/membrane/Object.getOwnPropertyDescriptor.js");
+
+// Object.getOwnPropertyNames
+// TODO: Not tested
+// load("test/membrane/Object.getOwnPropertyNames.js");
+
+// Object.has
+load("test/membrane/Object.has.js");
+
+// Object.hasOwn
+load("test/membrane/Object.hasOwn.js");
+
+// Object.isExtensible
+// Note: Proxy Trap does not exist
+load("test/membrane/Object.isExtensible.js");
+
+// Object.isFrozen
+// Note: Proxy Trap does not exist
 //load("test/membrane/Object.isFrozen.js");
-//load("test/membrane/Object.isSealed.js");
-////load("test/membrane/Object.iterate.js");
-////load("test/membrane/Object.keys.js"); 
-////load("test/membrane/Object.preventExtensions.js");
-load("test/membrane/Object.seal.js");
-////load("test/membrane/Object.set.js");
+
+// Object.isSealed
+// Note: Proxy Trap does not exist
+// load("test/membrane/Object.isSealed.js");
+
+// Object.iterate
+// Note: Proxy Trap ist not implemented
+load("test/membrane/Object.iterate.js");
+
+// Object.keys
+load("test/membrane/Object.keys.js"); 
+
+// Object.preventExtensions
+load("test/membrane/Object.preventExtensions.js");
+
+// Object.seal
+// Note: Proxy Trap ist not implemented
+// load("test/membrane/Object.seal.js");
+
+// Object.set
+load("test/membrane/Object.set.js");
+
+// _         _             _         
+//| |__  ___| |_  __ ___ _(_)___ _ _ 
+//| '_ \/ -_) ' \/ _` \ V / / _ \ '_|
+//|_.__/\___|_||_\__,_|\_/|_\___/_|  
+
+// typeof proxy
+// TODO
+
+// proxy instanceof constructor
+// TODO
+
+// Object.getPrototypeOf
+// TODO: Not tested
+// load("test/membrane/Object.getPrototypeOf.js");
