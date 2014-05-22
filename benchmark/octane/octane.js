f3c400b511ed9183ffc8ac202c43c26e68d78380
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-22 19:22:23 +0000

    sync
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24396 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ benchmark/octane/octane.js
@@ -0,0 +1,22 @@
+/*
+ * TreatJS: Sandbox 
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ *
+ * Copyright (c) 2014, Proglang, University of Freiburg.
+ * http://proglang.informatik.uni-freiburg.de/treatjs/
+ * All rights reserved.
+ *
+ * Released under the MIT license
+ * http://proglang.informatik.uni-freiburg.de/treatjs/license
+ *
+ * Author Matthias Keil
+ * http://www.informatik.uni-freiburg.de/~keilr/
+ */
+
+var params = {
+  verbose:true,
+  out:ShellOut(),
+  decompile:true,
+  membrabe:true,
+  effect:true
+};
