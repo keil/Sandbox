46ff0b3f4dd5e0082d00132a3b1397cd1ae571d8
keilr <keilr@e3db86c2-31ce-0310-bca5-be3edd1b5589>
2014-05-27 15:16:19 +0000

    add new dmo file
    git-svn-id: https://proglang.informatik.uni-freiburg.de/svn/proglang/projects/jsproxy/branches/TreatJSSandbox@24435 e3db86c2-31ce-0310-bca5-be3edd1b5589

---
+++ demo/statistic.js
@@ -0,0 +1,34 @@
+
+/*
+var s = new Statistic();
+print(s);
+s.increment("BUH");
+s.increment("BUH");
+s.increment("BUH");
+s.increment("Lala");
+s.increment("L");
+s.increment("BUH");
+print(s);
+
+print(s.keys());
+print(s.get("L"));
+*/
+
+var sbxS = new Sandbox(this, {verbose:false, statistic:true, out:ShellOut()});
+
+var o = {};
+var of = function() {
+  print("Ich hasse mich!");
+}
+o.f=of;
+o.g=of;
+
+sbxS.apply(function() { 
+  o.f;o.f; o.f;o.f; o.f;o.f; o.f;o.f; o.f;o.f; o.f;o.f;o.g;
+  o.f();
+  o.g();
+}, {}, []);
+
+sbxS.statistic.keys.foreach(function(i, op) {
+  print(op+": "+sbxS.statistic.get(op));
+});
